64f6a991418f2e90a2fc82978e6e90ad42361541
abdelrahman <abdo.aly.k.m@gmail.com>
2024-05-05 23:59:00 +0300

    init

--- MEDLIFE/wsgi.py
+++ MEDLIFE/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Medlife project.
+WSGI config for MEDLIFE project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Medlife.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MEDLIFE.settings')
 
 application = get_wsgi_application()
