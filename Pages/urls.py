64cf6644d7a2792378b5f568ea47725b58900f8b
abdelrahman <abdo.aly.k.m@gmail.com>
2024-05-06 02:51:52 +0300

    adding templates

--- Pages/urls.py
+++ Pages/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path,include
 from . import views
+
 urlpatterns = [
-    path('',views.index,name='index'),
-    path('',views.index,name='about_us'),
-    path('',views.index,name='contact_us'),
-    path('',views.index,name='products'),
-    path('',views.index,name='log_in'),
-    path('',views.index,name='sign_up'),
-    path('',views.index,name='item'),
+    path('', views.index, name='index'),
+    path('about_us', views.about_us, name='about_us'),
+    path('contact_us', views.contact_us, name='contact_us'),
+    path('products', views.products, name='products'),
+    path('log_in', views.log_in, name='log_in'),
+    path('sign_up', views.sign_up, name='sign_up'),
+    path('item', views.item, name='item'),
 ]
